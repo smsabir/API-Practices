db621a0430334ad0a77e5610b2e40c1309473329
S M Abdullah Al Sabir <ssabir@ttu.ee>
2021-02-18 01:05:43 +0200

    exploring API with simple tasks

--- RestWalkthrough/script.js
+++ RestWalkthrough/script.js
@@ -11,9 +11,28 @@ function displayData(countries){
         const countryInfo = `
             <h3 class="country-name">${country.name}</h3>
             <p>${country.capital}</p>
+            <button onclick="displayDetails('${country.name}')">Details</button>
         `
         div.innerHTML = countryInfo;
         section.appendChild(div);
         
     });
  };
+
+ const displayDetails= name => {
+    const url = `https://restcountries.eu/rest/v2/name/${name}?fullText=true`
+    fetch(url)
+    .then(res => res.json())
+    .then(data => renderCountryInfo(data[0]));
+ }
+
+ const renderCountryInfo = country => {
+     const countryDiv = document.getElementById('countryDetails');
+
+     countryDiv.innerHTML = `
+        <h1>${country.name}</h1>
+        <p>Population: ${country.population}</p>
+        <p>Area: ${country.area}</p>
+        <img src="${country.flag}">
+     `
+ }
